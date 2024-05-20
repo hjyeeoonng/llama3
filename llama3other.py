c657d991664f87d3b095dcb28206571f29a9f6c2
hjyeeoonng <j10123@naver.com>
2024-05-20 17:47:10 +0900

    testcode

--- llama3other.py
+++ llama3other.py
@@ -6,5 +6,5 @@ model_id = "meta-llama/Meta-Llama-3-8B"
 pipeline = transformers.pipeline(
     "text-generation", model=model_id, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto"
 )
-out = pipeline("Hey how are you doing today?")
+out = pipeline("Who are you?")
 print(out)
